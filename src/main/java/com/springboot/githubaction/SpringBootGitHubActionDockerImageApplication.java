a21f91b4824fd5ca001fe458df58139743d53a8d
Mallesh-1991 <potnuri.mallesh@gmail.com>
2024-11-17 15:09:50 +0530

    first commit

---
+++ src/main/java/com/springboot/githubaction/SpringBootGitHubActionDockerImageApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.githubaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitHubActionDockerImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitHubActionDockerImageApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/springboot/githubaction/controller/MessageController.java
@@ -0,0 +1,18 @@
+package com.springboot.githubaction.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/MyApp")
+public class MessageController {
+
+	
+	@GetMapping("/message")
+	public String gettestDept() {	
+		
+		return "This is demo for Spring Boot CI CD using GitHub-Actions .......";
+	}
+}
