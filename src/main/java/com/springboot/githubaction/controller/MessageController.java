09faf8d5c9b74b729bf203f9c6e47d0e8a32fa45
Mallesh-1991 <potnuri.mallesh@gmail.com>
2025-03-29 19:18:55 +0530

    Added Loggers

--- src/main/java/com/springboot/githubaction/controller/MessageController.java
+++ src/main/java/com/springboot/githubaction/controller/MessageController.java
@@ -1,5 +1,7 @@
 package com.springboot.githubaction.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/MyApp")
 public class MessageController {
 
+	   private static final Logger log = LoggerFactory.getLogger(MessageController.class);
 	
 	@GetMapping("/message")
 	public String gettestDept() {	
 		
-		
+		log.info("In Controller .........");
 		return "This is demo for Spring Boot CI CD using GitHub-Actions..";
 	}
 }
